038c3c9d90ffef3a8859e20d3e66116d5056a306
Maksik <maxylover@mail.ru>
2025-07-18 22:56:54 +0300

    Rewroted the method randomFn according to DRY.

--- src/main/java/Animals/RandomFn.java
+++ src/main/java/Animals/RandomFn.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public interface RandomFn {
 	default double randomFn(double metersObstance){
 		Random random = new Random();
-		metersObstance = metersObstance - (metersObstance * 0.25) +
-			(metersObstance * 1.25 - (metersObstance - (metersObstance * 0.25))) * random.nextDouble();
-		return metersObstance;
+		double min = metersObstance - metersObstance * 0.25;
+		double max = metersObstance * 1.25;
+		metersObstance = min + (max - min) * random.nextDouble();
+		return Math.round(metersObstance * 100.0) / 100.0;
 	}
 }
